53dd95391f7386d49b106d713cbd2399a6f23064
VMagioli <202408555954@alunos.estacio.br>
2024-10-17 16:14:01 +0000

    versão 3 correção valor variaveis

--- xadrez.c
+++ xadrez.c
@@ -12,7 +12,7 @@ int main() {
     printf("Bispo na posição %d\n", bispo);
     
     //estrutura de repetição de movimentação da peça bispo
-    while (bispo <=5)
+    while (bispo <=4)
     {
         printf("diagonal direita, cima\n"); //imprime o resultado da movimentação da peça
         bispo++; //incrementa o valor da variável
