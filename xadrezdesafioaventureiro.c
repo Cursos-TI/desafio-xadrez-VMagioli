bd7447ecec77da8026b35e827d13bc9da2922ad1
VMagioli <202408555954@alunos.estacio.br>
2024-10-19 15:40:22 +0000

    inserção das duas estruturas for

---
+++ xadrezdesafioaventureiro.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+for (int cavalocima = 0; cavalocima < 1 ; cavalocima++)
+{
+    for (int cavalodireita = 0; cavalodireita < 3; cavalodireita++)
+    {
+        printf("direita\n");
+    }
+    printf("cima\n");
+}
+
+    return 0;
+}
