72501c4add39fdf417a5340a6c3bc2af7d85c16f
VMagioli <202408555954@alunos.estacio.br>
2024-10-19 15:52:20 +0000

    adição de segunda movimentação da peça cavalo diferente da anterior

--- xadrezdesafioaventureiro.c
+++ xadrezdesafioaventureiro.c
@@ -2,14 +2,29 @@
 
 int main() {
 
-for (int cavalocima = 0; cavalocima < 1 ; cavalocima++)
+int cavalocima, cavalodireita; //declaração de variáveis
+
+for (cavalocima = 0; cavalocima < 1 ; cavalocima++) //definição do primeiro movimento da peça
 {
-    for (int cavalodireita = 0; cavalodireita < 3; cavalodireita++)
+    while (cavalodireita < 3)//loop do segundo movimento
     {
-        printf("direita\n");
+        printf("direita\n");//imprime o resultado da movimentação da peça
+        cavalodireita++; //incrementa o valor da variável
     }
-    printf("cima\n");
-}
+       printf("cima\n");//imprime o resultado da movimentação da peça para completar a movimentação em L
+    }
+
+    printf("\n");
+
+for (cavalodireita = 0; cavalodireita < 1 ; cavalodireita++) //nova movimentação da peça
+{
+    while (cavalocima <= 3)
+    {
+        printf("cima\n");//imprime o resultado da movimentação da peça
+        cavalocima++; //incrementa o valor da variável
+    }
+       printf("direita\n");//imprime o resultado da movimentação da peça para completar a movimentação em L
+    }
 
     return 0;
 }
